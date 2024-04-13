19bc7b72bc044870a257d5a77e197afd414aa503
pyalexos <aligos@yandex.ru>
2024-04-13 20:59:44 +0300

    new structure for source

---
+++ source/main.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request, render_template, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__, template_folder=os.path.join(os.path.abspath("./"), "source/templates"))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect("/")
+
+    return render_template("result.html")
+
+
+if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = os.path.join(os.path.abspath("./"), "data")
+    app.run(port=8000)
